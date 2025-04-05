2ab5d531a5e4a3b12fbe5c2bf21e1035fb3dd725
Manzanas-MarkAngelo <appleapolmanzanas@gmail.com>
2025-04-05 15:47:10 +0800

    add calendar to admin dashboard

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -3,6 +3,12 @@ class AdminDashboardController < ApplicationController
 
   def index
     @admin = current_admin
+        # Get the start date from the params or use today's date by default
+        @start_date = params[:start_date] ? Date.parse(params[:start_date]) : Date.today
+
+        # Calculate previous and next month dates manually
+        @previous_month = @start_date.prev_month.beginning_of_month
+        @next_month = @start_date.next_month.beginning_of_month
   end
 
   private
